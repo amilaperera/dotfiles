6e87032dc7e1d3646ef291f643f1604939b527ce
Amila Perera <perera.amila@gmail.com>
2013-10-30 16:42:23 +0530

    change man viewing termcap colors

--- .bash/bash_func.sh
+++ .bash/bash_func.sh
@@ -533,12 +533,12 @@ cl()
 man()
 {
 	env LESS_TERMCAP_mb=$'\E[01;31m' \
-	LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+	LESS_TERMCAP_md=$'\E[01;34m' \
 	LESS_TERMCAP_me=$'\E[0m' \
 	LESS_TERMCAP_se=$'\E[0m' \
-	LESS_TERMCAP_so=$'\E[38;5;246' \
+	LESS_TERMCAP_so=$'\E[01;33;40m' \
 	LESS_TERMCAP_ue=$'\E[0m' \
-	LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+	LESS_TERMCAP_us=$'\E[01;04;35m' \
 	man "$@"
 }
 # }}}
