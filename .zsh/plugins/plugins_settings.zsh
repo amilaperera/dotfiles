7b9feafe8985a69142027449c262f08e3e4ce746
Amila Perera <perera.amila@gmail.com>
2013-11-15 23:57:28 +0530

    adding plugins

---
+++ .zsh/plugins/plugins_settings.zsh
@@ -0,0 +1,9 @@
+#############################################################
+# Author: Amila Perera
+# File Name: plugins_settings.zsh
+#############################################################
+
+# zsh-syntax-highlighting settings {{{
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
+ZSH_HIGHLIGHT_PATTERNS+=('rm *' 'fg=white,bold,bg=red')
+# }}}
