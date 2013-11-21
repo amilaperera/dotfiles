13c9d9f8ce7b68289c184fb2410fc5cf83b09ba5
Amila Perera <perera.amila@gmail.com>
2013-11-21 23:21:41 +0530

    changing color of user name if the root user in prompt

--- .zsh/zprompt.zsh
+++ .zsh/zprompt.zsh
@@ -26,10 +26,17 @@ promptinit
 # zstyle ':vcs_info:*:*' nvcsformats "%~" ""
 
 # extracted from http://tsdh.wordpress.com/2007/12/06/my-funky-zsh-prompt/
-# and tweaked accordingly
+# and tweaked according to personal preferece
 local op="("
 local cp=")"
-local user_host_path="${op}%B%F{magenta}%n%b%F{14}@%B%F{magenta}%m%b%F{14}:%B%F{yellow}%~%b%f${cp}"
+local username=$(whoami)
+local user_host_path=
+if (( $UID == 0)); then
+	username=$(echo $username | tr 'a-z' 'A-Z')
+	user_host_path="${op}%B%F{red}$username%b%F{14}@%B%F{magenta}%m%b%F{14}:%B%F{yellow}%~%b%f${cp}"
+else
+	user_host_path="${op}%B%F{magenta}$username%b%F{14}@%B%F{magenta}%m%b%F{14}:%B%F{yellow}%~%b%f${cp}"
+fi
 local hist_no="${op}%F{12}%h%f${cp}"
 local smiley="%(?,%{$fg[green]%}✓%{$reset_color%},%{$fg[red]%}✗%{$reset_color%})"
 
