7683ddd4758a34f1d332011c026e6a53e54dd3ba
Amila Perera <perera.amila@gmail.com>
2014-03-06 05:24:58 +0530

    basic yaml reading

--- scripts/fork_sync.rb
+++ scripts/fork_sync.rb
@@ -4,5 +4,17 @@
 require 'yaml'
 
 class ForkSync
-
+  def initialize(&block)
+    @forks = block.call
+  end
 end
+
+fork_sync = ForkSync.new do
+  begin
+    YAML.load_file("_fork_sync3_.yaml")
+  rescue Exception => e
+    abort "can't open yaml file [ #{e.message} ]"
+  end
+end
+
+puts fork_sync.inspect
