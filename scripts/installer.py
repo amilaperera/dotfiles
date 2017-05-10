b06397934429f8afe8e9b410bceb26e63d2fff27
Amila Perera <perera.amila@gmail.com>
2017-05-11 00:08:30 +1000

    Initial commit of a python script to install necessary packages.

---
+++ scripts/installer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class DistroProber(object):
+    """DistroProber"""
+    def __init__(self, arg):
+        self.arg = arg
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
