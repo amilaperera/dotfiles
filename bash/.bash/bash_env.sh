1a36b7cff6380516b8496c0ba96e819d400e1701
Amila Perera <perera.amila@gmail.com>
2022-04-15 22:34:24 +1000

    ssh setup

--- bash/.bash/bash_env.sh
+++ bash/.bash/bash_env.sh
@@ -132,3 +132,32 @@ set -o ignoreeof    # Dont let Ctrl+D exit the shell
 set -o noclobber    # prevents overwriting existing files
 set -o vi           # specifies vi editing mode for command-line editing
 
+# Start SSH Agent
+# Reference: https://gist.github.com/bsara/5c4d90db3016814a3d2fe38d314f9c23
+# TODO: ~/.ssh/config should be setup in the bootstrap.sh
+GITHUB_ID="${HOME}/.ssh/id_github_personal"
+SSH_ENV="$HOME/.ssh/environment"
+
+function run_ssh_env {
+  . "${SSH_ENV}" > /dev/null
+}
+
+function start_ssh_agent {
+  echo "Initializing new SSH agent..."
+  ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+  echo "succeeded"
+  chmod 600 "${SSH_ENV}"
+
+  run_ssh_env
+
+  ssh-add "${GITHUB_ID}"
+}
+
+if [ -f "${SSH_ENV}" ]; then
+  run_ssh_env
+  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+    start_ssh_agent
+  }
+else
+  start_ssh_agent
+fi
