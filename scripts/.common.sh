66a5bf1bee56df8b4befd23327b903086dbe086a
Amila Perera <perera.amila@gmail.com>
2022-06-22 21:50:41 +1000

    Gdb builder

--- scripts/.common.sh
+++ scripts/.common.sh
@@ -9,93 +9,96 @@ YELLOW='\033[0;33m'
 NC='\033[0m'
 
 function yellow() {
-  printf "${YELLOW}$@${NC}\n"
+    printf "${YELLOW}$@${NC}\n"
 }
 
 function red() {
-  printf "${RED}$@${NC}\n"
+    printf "${RED}$@${NC}\n"
 }
 
 function green() {
-  printf "${GREEN}$@${NC}\n"
+    printf "${GREEN}$@${NC}\n"
 }
 
 function show_os_info() {
-  local os_name=`awk -F= '/^NAME/{print $2}' /etc/os-release 2> /dev/null`
-  local version=`awk -F= '/^\<VERSION\>/{print $2}' /etc/os-release 2> /dev/null`
-  local sanitized_version=`echo "$version" | tr -d '"'`
-  if [[ -n $os_name ]]; then
-    echo -e "Operating System: ${GREEN}${os_name} ${sanitized_version}${NC}"
-  else
-    echo -e "Operating System: ${RED}"Unknown"${NC}"
-  fi
+    local os_name=`awk -F= '/^NAME/{print $2}' /etc/os-release 2> /dev/null`
+    local version=`awk -F= '/^\<VERSION\>/{print $2}' /etc/os-release 2> /dev/null`
+    local sanitized_version=`echo "$version" | tr -d '"'`
+    if [[ -n $os_name ]]; then
+        echo -e "Operating System: ${GREEN}${os_name} ${sanitized_version}${NC}"
+    else
+        echo -e "Operating System: ${RED}"Unknown"${NC}"
+    fi
 }
 
 function export_install_command() {
-  if which dnf &> /dev/null; then
-    HAS_DNF=1
-    install_command="dnf install"
-  elif which apt-get &> /dev/null; then
-    HAS_APT=1
-    install_command="apt-get install"
-  elif which pacman &> /dev/null; then
-    HAS_PACMAN=1
-    install_command="pacman --noconfirm -S"
-  fi
+    if which dnf &> /dev/null; then
+        HAS_DNF=1
+        install_command="dnf install"
+    elif which apt-get &> /dev/null; then
+        HAS_APT=1
+        install_command="apt-get install"
+    elif which pacman &> /dev/null; then
+        HAS_PACMAN=1
+        install_command="pacman --noconfirm -S"
+    fi
 
-  if [[ -n $install_command ]]; then
-    echo -e "Install Command: ${GREEN}${install_command}${NC}"
-  else
-    echo -e "Install Command: ${RED}"Unknown"${NC}"
-    exit 1
-  fi
+    if [[ -n $install_command ]]; then
+        echo -e "Install Command: ${GREEN}${install_command}${NC}"
+    else
+        echo -e "Install Command: ${RED}"Unknown"${NC}"
+        exit 1
+    fi
 }
 
 function probe_os_info() {
-  yellow "Probing OS information"
-  show_os_info
-  export_install_command
-  echo
+    yellow "Probing OS information"
+    show_os_info
+    export_install_command
+    echo
 }
 
 function install() {
-  local cmd=
-  [[ $HAS_PACMAN -eq 1 ]] && cmd=`echo "sudo ${install_command} ${@}"` || cmd=`echo "sudo ${install_command} ${@} -y"`
-  echo $cmd
-  sh -c "$cmd"
+    local cmd=
+    [[ $HAS_PACMAN -eq 1 ]] && cmd=`echo "sudo ${install_command} ${@}"` || cmd=`echo "sudo ${install_command} ${@} -y"`
+    echo $cmd
+    sh -c "$cmd"
 }
 
 function pre_requisites() {
-  local pkgs=()
-  if [[ $HAS_APT -eq 1 ]]; then
-    pkgs+=(build-essential)
-    pkgs+=(libgmp-dev)
-    pkgs+=(libmpfr-dev)
-    pkgs+=(libmpc-dev)
-    pkgs+=(libisl-dev)
-    pkgs+=(libzstd-dev)
-    pkgs+=(ninja-build)
-    pkgs+=(git)
-  fi
-  install ${pkgs[*]}
+    local pkgs=()
+    if [[ $HAS_APT -eq 1 ]]; then
+        pkgs+=(build-essential)
+        pkgs+=(libgmp-dev)
+        pkgs+=(libmpfr-dev)
+        pkgs+=(libmpc-dev)
+        pkgs+=(libisl-dev)
+        pkgs+=(libzstd-dev)
+        pkgs+=(ninja-build)
+        pkgs+=(git)
+    fi
+    # gdb specific
+    pkgs+=(texinfo)
+    pkgs+=(gmp-devel)
+    install ${pkgs[*]}
 }
 
 # Function wrapper to install packages
 function install_packages() {
-  yellow "Installing ${@}..."
-  ${@}
-  echo
+    yellow "Installing ${@}..."
+    ${@}
+    echo
 }
 
 function die() {
-  red "Error: ${@}..."
-  exit
+    red "Error: ${@}..."
+    exit
 }
 
 function die_if_error {
-  if [[ $1 -ne 0 ]]; then
-    shift
-    die "$@"
-  fi
+    if [[ $1 -ne 0 ]]; then
+        shift
+        die "$@"
+    fi
 }
 
---
+++ scripts/install_gdb.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# gdb installation manual very clearly says that it relies on a POSIX
+# compliant shell to do the building and installtion. It very specifically
+# mentions that zsh will not work with the installation.
+# Therefore this particular script is run using bash (look at the shebang!!!)
+
+
+# utilities
+source .common.sh
+
+########################################
+# main
+########################################
+
+# Parse gdb version
+if [[ -z ${1} ]]; then
+    echo "install_gdb.sh <VERSION>"
+    die "No gdb version supplied"
+fi
+
+version=${1}
+
+# OS infomation probing
+probe_os_info
+
+# Install pre-requisites
+install_packages pre_requisites
+
+# Working directory
+cwd=${HOME}/work
+
+# Project directory
+project_directory=${cwd}/binutils-gdb
+
+# If project_directory doesn't exist create it and clone the project.
+# Otherwise this script assumes that the project has already been cloned.
+if [[ ! -e ${project_directory} ]]; then
+    # Clone the repository
+    mkdir -p ${cwd}
+    cd ${cwd} && git clone https://github.com/bminor/binutils-gdb
+    die_if_error $? "Cloning binutils-gdb failed"
+else
+    cd ${project_directory} && git co master && git clean -dfx
+fi
+
+cd ${project_directory} && git pull
+die_if_error $? "Pulling failed"
+
+# Work out the branch with the version provided
+branch='${version}'
+if [[ "$version" != "master" ]]; then
+    branch="gdb-${version}-release"
+fi
+
+cd ${project_directory} && git checkout ${branch}
+die_if_error $? "Checking out ${branch} failed"
+
+# Prepare build directory
+build_dir=${project_directory}/build
+# Fresh build each time!!!!
+if [[ -d "${build_dir}" ]]; then
+    rm -rf "${build_dir}"
+fi
+die_if_error $? "Removing the existing build directory failed"
+
+mkdir -p "${build_dir}"
+die_if_error $? "Creating the build directory failed"
+
+# configure
+cd ${build_dir} && ../configure --prefix=$HOME/.local/gdb-${version}
+
+die_if_error $? "Configuration failed"
+
+# make
+cd ${build_dir} && make -j8
+die_if_error $? "make failed"
+
+# make install
+cd ${build_dir} && make install -j8
+die_if_error $? "make install failed"
+
+# epilogue
+
+echo
+green "gdb-${version} installation successful"
+echo
+echo   "Bye..."
+
