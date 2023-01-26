ad941af654b237570e399076c40e573868fa7d88
Amila Perera <perera.amila@gmail.com>
2023-01-26 15:42:39 +1100

    More fzf-lua stuff

--- nvim.lua/after/plugin/fzf.lua
+++ nvim.lua/after/plugin/fzf.lua
@@ -11,6 +11,9 @@ vim.api.nvim_set_keymap('n', '<leader>f', "<cmd>lua require('fzf-lua').files()<C
 vim.api.nvim_set_keymap('n', '<C-t>', "<cmd>lua require('fzf-lua').git_files()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>c', "<cmd>lua require('fzf-lua').git_commits()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>b', "<cmd>lua require('fzf-lua').buffers()<CR>", { noremap = true, silent = true })
+-- most recently used
 vim.api.nvim_set_keymap('n', '<leader>m', "<cmd>lua require('fzf-lua').oldfiles()<CR>", { noremap = true, silent = true })
+-- search
 vim.api.nvim_set_keymap('n', '<leader>s', "<cmd>lua require('fzf-lua').live_grep()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>g', "<cmd>lua require('fzf-lua').live_grep({ cmd = 'git grep --line-number --column --color=always' })<CR>", {noremap = true, silent = true})
+-- git project search
+vim.api.nvim_set_keymap('n', '<leader>ps', "<cmd>lua require('fzf-lua').live_grep({ cmd = 'git grep --line-number --column --color=always' })<CR>", {noremap = true, silent = true})
