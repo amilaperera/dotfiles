d17c60e913f3e216a31e6e00b90a1321d4cf056d
Amila Perera <perera.amila@gmail.com>
2023-01-28 11:27:48 +1100

    More options

--- nvim.lua/lua/mappings.lua
+++ nvim.lua/lua/mappings.lua
@@ -11,7 +11,14 @@ vim.keymap.set('n', '<Left>', '<cmd>vertical resize -1<CR>', { silent = true })
 vim.keymap.set('n', '<Right>', '<cmd>vertical resize +1<CR>', { silent = true })
 
 -- get the full path of the file in the current buffer
-vim.keymap.set('n', '<Leader><Space>', function() print(vim.fn.expand('%:p')) end)
+vim.keymap.set('n', '<Leader><Space>', function()
+  print(vim.fn.expand('%:p'))
+end)
+-- change to the directory of the current buffer
+vim.keymap.set('n', 'cd', function()
+  vim.cmd([[cd %:p:h]])
+  print(vim.cmd[[pwd]])
+end)
 
 -- switching tabs made easy
 vim.keymap.set('n', '<Leader>1', '1gt')
@@ -23,3 +30,15 @@ vim.keymap.set('n', '<Leader>6', '6gt')
 vim.keymap.set('n', '<Leader>7', '7gt')
 vim.keymap.set('n', '<Leader>8', '8gt')
 vim.keymap.set('n', '<Leader>9', '9gt')
+
+-- moving visual blocks up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- scroll up and down focussing the center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- retaining current search always at the center
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
--- nvim.lua/lua/options.lua
+++ nvim.lua/lua/options.lua
@@ -1,22 +1,27 @@
 local o = vim.opt
 
+o.guicursor = ""
+
 o.tabstop = 4
 o.softtabstop = 4
 o.shiftwidth = 4
 o.expandtab = true
 
-o.number = true
-o.relativenumber = true
+o.smartindent = true
 
+o.hlsearch = true
+o.incsearch = true
 o.ignorecase = true
 o.smartcase = true
+
+o.number = true
+o.relativenumber = true
 
 o.cursorline = true
 
 o.swapfile = false
 o.updatetime = 250
 
-o.autoindent = true
 o.foldenable = false
 
 o.wildignorecase = true
@@ -25,4 +30,6 @@ o.spelllang = 'en'
 o.spell = true
 
 o.textwidth = 120
+o.formatoptions:append('t')
+
 o.scrolloff = 2
