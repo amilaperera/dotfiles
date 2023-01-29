8917ce1b3063799e41f3013b14711893c6dd3c01
Amila Perera <perera.amila@gmail.com>
2023-01-29 11:14:21 +1100

    More lsp tweaks

--- nvim.lua/after/plugin/lsp.lua
+++ nvim.lua/after/plugin/lsp.lua
@@ -1,6 +1,23 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.set_preferences({
+    sign_icons = {}
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
 lsp.configure('clangd', {
     cmd = { 'clangd', '-j=4' }
 })
