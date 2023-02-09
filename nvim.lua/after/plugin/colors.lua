bdd5fd819812511f38ddd81a7e09dc8e9655d4c5
Amila Perera <perera.amila@gmail.com>
2023-02-09 22:16:29 +1100

    colorscheme customization (diff view)

--- nvim.lua/after/plugin/colors.lua
+++ nvim.lua/after/plugin/colors.lua
@@ -3,6 +3,11 @@ vim.cmd([[let g:edge_disable_italic_comment = 1]])
 vim.o.background = "dark"
 vim.cmd.colorscheme("edge")
 
+-- More traditional colors for diff to be consistent across many terminals
+vim.cmd([[highlight DiffAdd guibg=#005f00]])
+vim.cmd([[highlight DiffDelete guibg=#af0000]])
+vim.cmd([[highlight DiffChange guibg=#878700]])
+
 -- restoring colors if they're cleared by the vim colorscheme
-vim.cmd([[hi debugPC term=reverse ctermbg=yellow guibg=yellow]])
-vim.cmd([[hi debugBreakpoint term=reverse ctermbg=red guibg=red]])
+vim.cmd([[highlight debugPC term=reverse ctermbg=yellow guibg=yellow]])
+vim.cmd([[highlight debugBreakpoint term=reverse ctermbg=red guibg=red]])
