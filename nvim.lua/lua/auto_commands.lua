2a1951d524ad25428ea05ceba30eaf754f7e12bd
Amila Perera <perera.amila@gmail.com>
2023-02-20 23:35:50 +1100

    Minor

--- nvim.lua/lua/auto_commands.lua
+++ nvim.lua/lua/auto_commands.lua
@@ -1,17 +1,7 @@
--- conditionally enable spell checking
--- vim.api.nvim_create_augroup("SpellCheckGroup", { clear = true })
--- vim.api.nvim_create_autocmd({"FileType"}, {
-    -- pattern = {"c", "cpp", "markdown","python", "lua", "text"},
-    -- callback = function()
-        -- vim.api.nvim_win_set_option(0, "spell", true)
-    -- end,
-    -- group = "SpellCheckGroup"
--- })
-
 -- easily close temporary buffer windows like help, fugitive etc.
 vim.api.nvim_create_augroup("FastCloseGroup", { clear = true })
 vim.api.nvim_create_autocmd({"FileType"}, {
-    pattern = {"fugitive", "help"},
+    pattern = {"help", "fugitive", "fugitiveblame"},
     callback = function()
         vim.keymap.set('n', 'q', '<cmd>close<CR>', { silent = true, buffer = true })
     end,
