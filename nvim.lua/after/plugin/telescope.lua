869e846a7ca7986b903670f30092daac0faba2b4
Amila Perera <perera.amila@gmail.com>
2023-02-26 22:35:47 +1100

    Minor

--- nvim.lua/after/plugin/telescope.lua
+++ nvim.lua/after/plugin/telescope.lua
@@ -67,33 +67,33 @@ end
 
 local builtin = require("telescope.builtin")
 -- keymaps
-vim.keymap.set('n', 'T', '<cmd>Telescope<CR>', { desc = 'Invoke telescope'})
-vim.keymap.set('n', '<C-t>', builtin.git_files, { desc = ''})
-vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recentlyl opened files' })
-vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = '[,] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = '[/] Fuzzily search current buffer' })
+vim.keymap.set('n', 'T', '<cmd>Telescope<CR>', {desc = 'Invoke telescope'})
+vim.keymap.set('n', '<leader><Space>', builtin.git_files, {desc = ''})
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, {desc = '[?] Find recentlyl opened files'})
+vim.keymap.set('n', '<leader>,', builtin.buffers, {desc = '[,] Find existing buffers'})
+vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, {desc = '[/] Fuzzily search current buffer'})
 
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sc', builtin.git_commits, { desc = '[S]earch git [C]ommits' })
+vim.keymap.set('n', '<leader>sf', builtin.find_files, {desc = '[S]earch [F]iles'})
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, {desc = '[S]earch [H]elp'})
+vim.keymap.set('n', '<leader>sc', builtin.git_commits, {desc = '[S]earch git [C]ommits'})
 
 -- Search by grep (Really handy if you use C-R C-W to search the word under cursor)
 vim.keymap.set('n', '<leader>sg', function()
     local root = git_root_of_current_buffer()
     builtin.grep_string({cwd = root, search = vim.fn.input("Grep > ") })
-end, { desc = '[S]earch by [G]rep' })
+end, {desc = '[S]earch by [G]rep'})
 
 -- project live grep
 vim.keymap.set('n', '<leader>sps', function()
     local root = git_root_of_current_buffer()
     builtin.live_grep({cwd = root})
-end, { desc = '[S]earch [P]roject (case [S]ensitive)' })
+end, {desc = '[S]earch [P]roject (case [S]ensitive)'})
 
 -- project live grep (case insensitive)
 vim.keymap.set('n', '<leader>spi', function()
     local root = git_root_of_current_buffer()
     builtin.live_grep({cwd = root, additional_args = {"-i"}})
-end, { desc = '[S]earch [P]roject (case [I]nsensitive)' })
+end, {desc = '[S]earch [P]roject (case [I]nsensitive)'})
 
 
 -- explore configs
