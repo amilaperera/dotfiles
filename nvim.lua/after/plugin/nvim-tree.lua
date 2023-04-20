edbd6266c20c6385aa13858229b80c161f31796a
Amila Perera <perera.amila@gmail.com>
2023-04-20 18:15:02 +1000

    Handle deprecated apis

--- nvim.lua/after/plugin/nvim-tree.lua
+++ nvim.lua/after/plugin/nvim-tree.lua
@@ -2,16 +2,19 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function on_attach(bufnr)
+    local api = require('nvim-tree.api')
+
+    local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+    vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
+end
+
 -- Setup with some options
 -- Desperately trying to bring NvimTree closer to plain old (but yet good) NerdTree
 require("nvim-tree").setup({
-    view = {
-        mappings = {
-            list = {
-                { key = "u", action = "dir_up" },
-            },
-        },
-    },
+    on_attach = on_attach,
     renderer = {
         icons = {
             symlink_arrow = '➛',
