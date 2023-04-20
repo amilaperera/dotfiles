0cd0e52326581706dc9c67529eb602a5bac49096
Amila Perera <perera.amila@gmail.com>
2023-04-20 21:32:13 +1000

    Adding default key mappings

--- nvim.lua/after/plugin/nvim-tree.lua
+++ nvim.lua/after/plugin/nvim-tree.lua
@@ -8,6 +8,7 @@ local function on_attach(bufnr)
     local function opts(desc)
         return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
     end
+    api.config.mappings.default_on_attach(bufnr)
     vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
 end
 
