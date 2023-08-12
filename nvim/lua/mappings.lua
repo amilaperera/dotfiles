44b6d5f51f61c45b82710782016fb5ddd9666f2d
Amila Perera <perera.amila@gmail.com>
2023-08-12 22:12:21 +1000

    Few toggles

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -16,6 +16,11 @@ vim.keymap.set('n', 'cd', function()
     vim.fn.chdir(vim.fn.expand('%:p:h'))
     print("Changed to: " .. vim.fn.getcwd())
 end)
+-- change to git directory
+vim.keymap.set('n', 'gcd', function()
+    vim.cmd([[Gcd]])
+    print("Changed to: " .. vim.fn.getcwd())
+end)
 
 -- switching tabs made easy
 for i = 1,9 do
@@ -25,9 +30,13 @@ vim.keymap.set('n', '<C-Left>', ":tabprevious<CR>")
 vim.keymap.set('n', '<C-Right>', ":tabnext<CR>")
 vim.keymap.set('n', '<Leader>xt', ":tabclose<CR>")
 
--- moving visual blocks up and down
+-- Moving visual blocks up and down.
+-- multiple lines in visual mode
 vim.keymap.set('v', ']e', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', '[e', ":m '<-2<CR>gv=gv")
+-- current line normal mode
+vim.keymap.set('n', ']e', "ddp")
+vim.keymap.set('n', '[e', "ddkP")
 
 -- scroll up and down focussing the center
 vim.keymap.set('n', "<C-d>", "<C-d>zz")
@@ -36,6 +45,40 @@ vim.keymap.set('n', "<C-u>", "<C-u>zz")
 -- open $MYVIMRC in a new tab
 vim.keymap.set('n', "<Leader>v", ":tabe $MYVIMRC<CR>")
 
--- File full path. Gives better information than Ctrl_G
+-- File full path. If you're unhappy with Ctrl_G output
 vim.keymap.set('n', "<Space>", function() print(vim.fn.expand('%:p')) end, { silent = true })
+
+local toggle_option = function(option)
+    if vim.opt[option]:get() then
+        vim.opt[option] = false
+    else
+        vim.opt[option] = true
+    end
+end
+
+local toggle_cursor_options = function(opt1, opt2)
+    if vim.opt[opt1]:get() and vim.opt[opt2]:get() then
+        vim.opt[opt1] = false
+        vim.opt[opt2] = false
+    else
+        vim.opt[opt1] = true
+        vim.opt[opt2] = true
+    end
+end
+
+-- trying to emulate some of Tim Pope's unimpaired toggles
+vim.keymap.set('n', "yoc", function() toggle_option('cursorline') end, { silent = true })
+vim.keymap.set('n', "you", function() toggle_option('cursorcolumn') end, { silent = true })
+vim.keymap.set('n', "yoh", function() toggle_option('hlsearch') end, { silent = true })
+vim.keymap.set('n', "yoi", function() toggle_option('ignorecase') end, { silent = true })
+vim.keymap.set('n', "yon", function() toggle_option('number') end, { silent = true })
+vim.keymap.set('n', "yor", function() toggle_option('relativenumber') end, { silent = true})
+vim.keymap.set('n', "yos", function() toggle_option('spell') end, { silent = true })
+vim.keymap.set('n', "yow", function() toggle_option('wrap') end, { silent = true })
+
+vim.keymap.set(
+    'n',
+    "yox",
+    function() toggle_cursor_options('cursorline', 'cursorcolumn') end,
+    { silent = true })
 
