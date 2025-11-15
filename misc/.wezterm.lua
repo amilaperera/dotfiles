f10c0e1597e19002ed5de34c966c98b0c5c75b74
Amila Perera <perera.amila@gmail.com>
2025-11-15 18:53:27 +1100

    Minor

--- misc/.wezterm.lua
+++ misc/.wezterm.lua
@@ -11,29 +11,29 @@ config.launch_menu = launch_menu
 
 -- For example, changing the initial geometry for new windows:
 if wezterm.target_triple:find("windows") then
-	config.default_domain = "WSL:Ubuntu"
-	config.font = wezterm.font("FiraCode Nerd Font")
-	config.font_size = 10
-	config.color_scheme = "Konsolas"
+    config.default_domain = "WSL:Ubuntu"
+    config.font = wezterm.font("FiraCode Nerd Font")
+    config.font_size = 10
+    config.color_scheme = "Konsolas"
 else
-	config.font = wezterm.font("Hack Nerd Font")
-	config.font_size = 10
-	config.default_domain = "local"
-	config.color_scheme = "Catppuccin Mocha"
+    config.font = wezterm.font("Hack Nerd Font")
+    config.font_size = 10
+    config.default_domain = "local"
+    config.color_scheme = "Catppuccin Mocha"
 end
 
 config.window_background_opacity = 0.9
 
 config.colors = {
-	cursor_bg = "white",
+    cursor_bg = "white",
 }
 config.default_cursor_style = "SteadyBlock"
 
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function()
-	local tab, pane, window = mux.spawn_window({})
-	window:gui_window():maximize()
+    local tab, pane, window = mux.spawn_window({})
+    window:gui_window():maximize()
 end)
 
 -- Finally, return the configuration to wezterm:
