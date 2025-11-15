3b6ab5ef2dc968e198f871f6312521eda39840d7
Amila Perera <perera.amila@gmail.com>
2025-11-16 09:23:28 +1100

    Minor

--- misc/.wezterm.lua
+++ misc/.wezterm.lua
@@ -1,16 +1,10 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-local mux = wezterm.mux
-
-local launch_menu = {}
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-config.launch_menu = launch_menu
-
--- For example, changing the initial geometry for new windows:
+-- platform specific settings
 if wezterm.target_triple:find("windows") then
     config.default_domain = "WSL:Ubuntu"
     config.font = wezterm.font("FiraCode Nerd Font")
@@ -23,21 +17,30 @@ else
     config.color_scheme = "nightfox"
 end
 
+-- window settings
 config.window_decorations = "RESIZE"
-
-config.colors = {
-    cursor_bg = "white",
+config.window_padding = {
+    left = 5,
+    right = 5,
+    top = 0,
+    bottom = 0,
 }
 
-config.default_cursor_style = "SteadyBlock"
-config.term = "wezterm"
-
-config.use_fancy_tab_bar = false
-
+-- maximize window on startup
+local mux = wezterm.mux
 wezterm.on("gui-startup", function()
     local tab, pane, window = mux.spawn_window({})
     window:gui_window():maximize()
 end)
+
+-- cursor settings
+config.default_cursor_style = "SteadyBlock"
+config.colors = {
+    cursor_bg = "white",
+}
+
+-- tab bar settings
+config.use_fancy_tab_bar = false
 
 -- Finally, return the configuration to wezterm:
 return config
