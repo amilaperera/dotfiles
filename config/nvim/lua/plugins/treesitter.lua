6815d53d17734416736e7a0b78890d28fb599c71
Amila Perera <perera.amila@gmail.com>
2026-09-18 23:06:30 +1000

    tree-sitter related changes

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,8 @@ return {
         build = ":TSUpdate",
         branch = "main",
         -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
-        config = function()
-            local parsers = {
+        opts = {
+            ensure_installed = {
                 "bash",
                 "c",
                 "cpp",
@@ -22,37 +22,11 @@ return {
                 "tmux",
                 "vim",
                 "vimdoc",
-            }
-
-            if vim.fn.has("win32") == 0 then
-                require("nvim-treesitter").install(parsers)
-            end
-
-            vim.api.nvim_create_autocmd("FileType", {
-                callback = function(args)
-                    local buf, filetype = args.buf, args.match
-
-                    local language = vim.treesitter.language.get_lang(filetype)
-                    if not language then
-                        return
-                    end
-
-                    -- check if parser exists and load it
-                    if not vim.treesitter.language.add(language) then
-                        return
-                    end
-                    -- enables syntax highlighting and other treesitter features
-                    vim.treesitter.start(buf, language)
-
-                    -- enables treesitter based folds
-                    -- for more info on folds see `:help folds`
-                    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-                    -- vim.wo.foldmethod = 'expr'
-
-                    -- enables treesitter based indentation
-                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                end,
-            })
-        end,
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        },
     },
 }
