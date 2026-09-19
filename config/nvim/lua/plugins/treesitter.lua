b9f9561677e058f692e7af4bf4cba484963cf54f
Amila Perera <perera.amila@gmail.com>
2026-09-19 12:50:01 +1000

    Reinstating tree-sitter to avoid highlight issues

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,8 @@ return {
         build = ":TSUpdate",
         branch = "main",
         -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
-        opts = {
-            ensure_installed = {
+        config = function()
+            local parsers = {
                 "bash",
                 "c",
                 "cpp",
@@ -19,14 +19,39 @@ return {
                 "markdown_inline",
                 "python",
                 "query",
-                "tmux",
                 "vim",
                 "vimdoc",
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        },
+            }
+
+            if vim.fn.has("win32") == 0 then
+                require("nvim-treesitter").install(parsers)
+            end
+
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local buf, filetype = args.buf, args.match
+
+                    local language = vim.treesitter.language.get_lang(filetype)
+                    if not language then
+                        return
+                    end
+
+                    -- check if parser exists and load it
+                    if not vim.treesitter.language.add(language) then
+                        return
+                    end
+                    -- enables syntax highlighting and other treesitter features
+                    vim.treesitter.start(buf, language)
+
+                    -- enables treesitter based folds
+                    -- for more info on folds see `:help folds`
+                    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                    -- vim.wo.foldmethod = 'expr'
+
+                    -- enables treesitter based indentation
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+        end,
     },
 }
